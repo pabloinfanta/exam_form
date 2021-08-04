7e91c76d98f7b0dbc99dd7e248f0f8faa49b10c0
Pablo <pablo.infanta.z@gmail.com>
2021-08-04 19:16:03 -0400

    se cambio correo destinatario a evaluaciones@gesalud.cl

--- app/mailers/appointment_mailer.rb
+++ app/mailers/appointment_mailer.rb
@@ -1,6 +1,6 @@
 class AppointmentMailer < ApplicationMailer
   def appointment_email(info)
     @info = info
-    mail(to: 'reservas.gesalud@gmail.com', subject: 'Agendamiento '"#{@info['company']}")
+    mail(to: 'evaluaciones@gesalud.cl', subject: 'Agendamiento '"#{@info['company']}")
   end
 end
